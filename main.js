4938423c211e7a753a079444ace9b4193bc0e395
Sergo_Kaz <wwe619dx@ukr.net>
2025-03-24 21:47:33 +0100

    add new styles

--- main.js
+++ main.js
@@ -8,6 +8,21 @@ import showAnimes from "./module/showAnimes.js";
 const filterBtn = document.querySelector(".filterBtn");
 filterBtn.addEventListener("click", addFilter);
 
+const genresBlockBtn = document.querySelector(".genresBlockBtn");
+genresBlockBtn.addEventListener("click", (e) => {
+  const genresSelectBlock = document.querySelector(".genresSelectBlock");
+  genresSelectBlock.classList.toggle("genresSelectBlockShow");
+
+  const genresSelect = document.querySelector(".genresSelect");
+  const hBlock = genresSelect.clientHeight;
+  const root = document.documentElement;
+
+  root.style.setProperty("--h", hBlock + "px");
+
+  const rotateGenresBtn = document.querySelector(".genresBlockBtn");
+  rotateGenresBtn.classList.toggle("rotateBtn");
+});
+
 getApiData().then((anime) => showAnimes(anime));
 
 // ===========================================================
--- module/addFilter.js
+++ module/addFilter.js
@@ -1,6 +1,14 @@
 function addFilter() {
   const filterWindow = document.querySelector("#filter");
   filterWindow.classList.toggle("showFilter");
+
+  const genresSelectBlock = document.querySelector(".genresSelectBlock");
+  console.log(["genresSelect"], genresSelectBlock);
+
+  if (genresSelectBlock.classList.contains("genresSelectBlockShow")) {
+    genresSelectBlock.classList.remove("genresSelectBlockShow");
+    document.querySelector(".genresBlockBtn").classList.remove("rotateBtn");
+  }
 }
 
 export default addFilter;
